4616dcd04fc0b502a02688f41630b88d4d54cf8a
Adrian Buenache <arhacai@gmail.com>
2018-10-28 21:12:44 +0100

    Added documentation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# matrices
+# Matrices
+
+A simple module to work with matrices. You can create a Matrix object and do elemental operations with it, like sum, sub, multiply... You can also calculate its determinant, tranpose, adjoint or even inverse matrix.
+
+
--- matrices.py
+++ matrices.py
@@ -1,5 +1,6 @@
 
 def determinant(matrix):
+    """Returns the determinant of the matrix given."""
     if matrix.rows != matrix.columns:
         raise IndexError(
             "Matrix determinant can only be calculated on square matrices."
@@ -13,6 +14,7 @@ def determinant(matrix):
 
 
 def identity(size):
+    """Returns a Matrix object with and identity matrix of the size given."""
     result = []
     for row in range(size):
         result.append([])
@@ -25,6 +27,7 @@ def identity(size):
 
 
 def minor(matrix, i, j):
+    """Returns the minor for the row i, column j of the matrix given."""
     from copy import deepcopy
     matrix = deepcopy(matrix.matrix)
     for row in matrix:
@@ -34,10 +37,14 @@ def minor(matrix, i, j):
 
 
 def adjoint(matrix, row, column):
+    """
+    Returns the adjoint element for the row and column on the given matrix.
+    """
     return pow(-1, row+column) * minor(matrix, row, column)
 
 
 def adjoint_matrix(matrix):
+    """Returns the adjoint matrix of the one provided."""
     result = []
     for row in range(matrix.rows):
         result.append([])
@@ -47,6 +54,11 @@ def adjoint_matrix(matrix):
 
 
 def inverse(matrix):
+    """
+    Calculates and returns the inverse matrix, if exists, of the matrix
+    given. It raises an Error if determinant of given matrix is 0 or if the
+    matrix given is not a square one.
+    """
     if matrix.rows != matrix.columns:
         raise ValueError(
             "Inverse matrix can only be calculated on square matrices."
@@ -58,6 +70,7 @@ def inverse(matrix):
 
 
 def transpose(matrix):
+    """Returns the transpose matrix of the given one."""
     result = []
     for row in range(matrix.columns):
         result.append([])
@@ -67,6 +80,11 @@ def transpose(matrix):
 
 
 class Matrix:
+    """
+    This class represents a matrix of m rows and n columns. It stores the
+    matrix itself into an 2D-array to opearte with, and the values of number
+    of rows and columns.
+    """
 
     def __init__(self, matrix=None):
         if matrix is None:
@@ -75,15 +93,29 @@ class Matrix:
 
     @staticmethod
     def get_values():
+        """
+        Allows Matrix creation with user input values. Input must be a
+        string made of numbers separates by a space for each column and a
+        semicolon and space for each row.
+        Example: '1 2 3; 2 0 -1; 3 2 2'
+        """
         values = input("Values? (Use '; ' to separate each row) ").split("; ")
         return [[int(column) for column in row.split(" ")] for row in values]
 
     def get_matrix(self, matrix):
+        """
+        Matrix must be a string or a list in order to get correct values.
+        Example (string): 1 2 3; 2 0 -1; 3 2 2'
+        Example (list): [[2, 1], [1, 0], [-3, -2]]
+        Returns a matrix as a list and its number of columns and rows
+        """
         if type(matrix) != str and type(matrix) != list:
             raise ValueError("You must introduce a matrix as a string or list")
         if type(matrix) == str:
             values = matrix.split("; ")
-            matrix = [[int(column) for column in row.split(" ")] for row in values]
+            matrix = [
+                [int(column) for column in row.split(" ")] for row in values
+            ]
         rows = len(matrix)
         columns = len(matrix[0])
         for row in matrix:
@@ -94,6 +126,7 @@ class Matrix:
         return matrix, rows, columns
 
     def element(self, row, column):
+        """Returns the row,column element of the matrix."""
         return self.matrix[row][column]
 
     def __add__(self, other):
@@ -164,32 +197,3 @@ class Matrix:
 
     def __eq__(self, other):
         return self.matrix == other.matrix
-
-
-def first_exercise():
-    # A = Matrix([[-1, 2, 4, 0], [3, 2, -1, -3], [6, 0, 1, 1]])
-    A = Matrix("-1 2 4 0; 3 2 -1 -3; 6 0 1 1")
-    B = Matrix([[2, 1], [1, 0], [-3, -2], [0, 3]])
-    print("First Exercise:")
-    print("a) A x B")
-    print(A*B)
-    print("\n(A x B)t = Br x At")
-    print(transpose(A*B) == transpose(B)*transpose(A))
-
-
-def third_exercise():
-    A = Matrix([[1, 4], [1, 3]])
-    B = Matrix([[4, 0], [1, 2]])
-
-    print("Third Exercise:")
-    print("a) AXA = AB")
-    print("Sol:")
-    print("A'AXAA' = AA'BA'")
-    print("IXI = IBA'")
-    print("X = BA'")
-    print(B * inverse(A))
-
-if __name__ == '__main__':
-
-    first_exercise()
-    third_exercise()
